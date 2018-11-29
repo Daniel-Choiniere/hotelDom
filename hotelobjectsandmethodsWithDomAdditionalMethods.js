d09166cc3cc224f3452d840177fe0b618eea7e23
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-11-29 02:16:05 +0000

    changed unordered list to drop down

---
+++ hotelobjectsandmethodsWithDomAdditionalMethods.js
@@ -0,0 +1,48 @@
+var hotel = {
+    name: "CareerDevs Mega Hotel",
+    rating: 5.0,
+    numberOfRooms: 5,
+    roomRate: 325.00,
+    roomNumbersAvailable: ["101", "102", "103", "104", "105", "106"],
+    roomNumbersBooked: [],
+    roomType: "Queen",
+    
+    numberOfRoomsAvailable: function() {
+        // length of the number of rooms available, returned as an integer (1,5,20, etc)
+        return this.roomNumbersAvailable.length;
+    },
+    
+    numberOfRoomsBooked: function() {
+        return this.roomNumbersBooked.length;
+    },
+    
+    numberOfRooms: function() {
+        return this.roomNumbersAvailable.length + this.roomNumbersBooked.length;
+    },
+    
+    bookRoom: function() {
+        if (this.numberOfRoomsAvailable() > 0) {
+            // select a random avaliable room
+            var randomRoom = this.roomNumbersAvailable[Math.floor(Math.random() * this.roomNumbersAvailable.length)];
+            this.roomNumbersAvailable.splice(this.roomNumbersAvailable.indexOf(randomRoom), 1);
+            this.roomNumbersBooked.push(randomRoom);
+        }
+    }
+};
+
+document.getElementById("hotelName").innerText = hotel.name;
+
+// rmsAvail
+var rmList = "<select>";
+for (var i=0; i < hotel.roomNumbersAvailable.length; i++) {
+    
+    rmList += "<option>" + hotel.roomNumbersAvailable[i] + "</option>";
+    
+}
+rmList += "</select>";
+
+document.getElementById("rmsAvail").innerHTML = rmList;
+
+
+// create a new method that uses the rmlist functionality to update the roomlist
+// change from unorderd list to drop down
