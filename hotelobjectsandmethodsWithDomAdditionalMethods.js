e9db265052ff07c95512580945c3c924ce19c115
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-07 12:04:22 +0000

    user can now checkout a room (moves room number from roomsbooked dropdown to rooms avail dropdown) and updates arrays

--- hotelobjectsandmethodsWithDomAdditionalMethods.js
+++ hotelobjectsandmethodsWithDomAdditionalMethods.js
@@ -1,5 +1,5 @@
 var hotel = {
-    name: "CareerDevs JavaShift Hotel",
+    name: "CareerDevs Java Hotel",
     rating: 5.0,
     numberOfRooms: 5,
     roomRate: 325.00,
@@ -22,19 +22,47 @@ var hotel = {
 
     bookRoom: function() {
         if (this.numberOfRoomsAvailable() > 0) {
-
             // Get selected room from user dropdown selection
             var roomPicked = document.getElementById("mySelect").value;
-            
             // Remove selected room from roomNumbersAvailable 
             // Push selected room into roomNumbersBooked
             this.roomNumbersBooked = this.roomNumbersBooked.concat(this.roomNumbersAvailable.splice(this.roomNumbersAvailable.indexOf(roomPicked), 1));
 
 
+            // // update rooms avail dropdown  // essentially removing the selected room from the room avail dropdowm
+            var rmList = "<form> <select id='mySelect'>";
+            for (var i = 0; i < this.roomNumbersAvailable.length; i++) {
+                rmList += '<option value="' + this.roomNumbersAvailable[i] + '">' + this.roomNumbersAvailable[i] + "</option>";
+            }
+            rmList += "</select> </form>";
+            document.getElementById("rmsAvail").innerHTML = rmList;
+
+            // update rooms BOOKED dropdown  // essentially takes the room taken from rooms avail and puts it into rooms booked dropwdown
+            var roomsBooked = "<form> <select id='bookedDropdown'>";
+            for (var i = 0; i < this.roomNumbersBooked.length; i++) {
+                roomsBooked += "<option value=" + this.roomNumbersBooked[i] + ">" + this.roomNumbersBooked[i] + "</option>";
+            }
+            roomsBooked += "</select> </form>";
+
+            document.getElementById("roomsBooked").innerHTML = roomsBooked;
+            document.getElementById("demo").innerHTML = "You will love room  " + roomPicked + " it is a wonderful choice.";
+
+
+        }
+    },
+    
+    checkoutRoom: function() {
+        // Get selected room from user dropdown selection
+            var roomReturned = document.getElementById("bookedDropdown").value;
+            
+            // Remove selected room fromroomNumbersBooked
+            // Push selected room into roomNumbersAvailable 
+            this.roomNumbersAvailable = this.roomNumbersAvailable.concat(this.roomNumbersBooked.splice(this.roomNumbersBooked.indexOf(roomReturned), 1));
+
+
             // update rooms avail dropdown  // essentially removing the selected room
             var rmList = "<form> <select id='mySelect'>";
             for (var i = 0; i < this.roomNumbersAvailable.length; i++) {
-
                 rmList += '<option value="' + this.roomNumbersAvailable[i] + '">' + this.roomNumbersAvailable[i] + "</option>";
             }
             rmList += "</select> </form>";
@@ -42,19 +70,14 @@ var hotel = {
             document.getElementById("rmsAvail").innerHTML = rmList;
 
             // update rooms BOOKED dropdown  // essentially removing the selected room
-            var roomsBooked = "<form> <select id='mySelect'>";
+            var roomsBooked = "<form> <select id='bookedDropdown'>";
             for (var i = 0; i < this.roomNumbersBooked.length; i++) {
-
                 roomsBooked += "<option value=" + this.roomNumbersBooked[i] + ">" + this.roomNumbersBooked[i] + "</option>";
             }
             roomsBooked += "</select> </form>";
-
-            document.getElementById("roomsBooked").innerHTML = roomsBooked;
             
-            //document.getElementById("demo").innerHTML = "You picked room " + roomPicked + ".";
-
-
-        }
+            document.getElementById("roomsBooked").innerHTML = roomsBooked;
+            document.getElementById("demo").innerHTML = "Thank you for your stay. I hope you have enjoyed room " + roomReturned + " Please come back soon!";
     }
 };
 
@@ -66,21 +89,17 @@ document.getElementById("hotelName").innerText = hotel.name;
 
 var rmList = "<form> <select id='mySelect'>";
 for (var i = 0; i < hotel.roomNumbersAvailable.length; i++) {
-
     rmList += '<option value="' + hotel.roomNumbersAvailable[i] + '">' + hotel.roomNumbersAvailable[i] + "</option>";
 }
 rmList += "</select> </form>";
-
 document.getElementById("rmsAvail").innerHTML = rmList;
 
 
 // Begin booked rooms select dropdown
 
-var roomsBooked = "<form> <select id='mySelect'>";
+var roomsBooked = "<form> <select id='bookedDropdown'>";
 for (var i = 0; i < hotel.roomNumbersBooked.length; i++) {
-
     roomsBooked += '<option value="' + hotel.roomNumbersBooked[i] + '">' + hotel.roomNumbersBooked[i] + "</option>";
 }
 roomsBooked += "</select> </form>";
-
 document.getElementById("roomsBooked").innerHTML = roomsBooked;
