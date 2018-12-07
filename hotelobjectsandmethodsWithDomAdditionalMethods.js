e95a239bbb69e1ef57aed275fd05ee74d038f9dc
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-07 00:20:38 +0000

    remove from dropdown once selected functionality

--- hotelobjectsandmethodsWithDomAdditionalMethods.js
+++ hotelobjectsandmethodsWithDomAdditionalMethods.js
@@ -32,27 +32,46 @@ var hotel = {
 
 document.getElementById("hotelName").innerText = hotel.name;
 
-
 // Select a user clicked specific room option from a dropdown
 function selectRoom() {
     // Get selected room form user dropdown selection
     var roomPicked = document.getElementById("mySelect").value;
     document.getElementById("demo").innerHTML = "You picked room " + roomPicked + ".";
     // Test 
-    console.log(roomPicked);
-    console.log(hotel.roomNumbersAvailable.indexOf(roomPicked));
+    // console.log(roomPicked);
+    // console.log(hotel.roomNumbersAvailable.indexOf(roomPicked));
     // Remove selected room from roomNumbersAvailable 
     // Push selected room into roomNumbersBooked
     hotel.roomNumbersAvailable.splice(hotel.roomNumbersAvailable.indexOf(roomPicked), 1);
     hotel.roomNumbersBooked.push(roomPicked);
     
-     // Remove item from dropdown
-    var roomPickedRemove = document.getElementById("mySelect");
-    roomPickedRemove.remove(roomPickedRemove.selectedIndex);
+    // Remove item from dropdown
+    var rmList = "<form> <select id='mySelect'>";
+    for (var i = 0; i < this.roomNumbersAvailable.length; i++) {
+
+        rmList += '<option value="' + this.roomNumbersAvailable[i] + '">' + this.roomNumbersAvailable[i] + "</option>";
+    }
+    rmList += "</select> </form>";
+
+    document.getElementById("rmsAvail").innerHTML = rmList;
     
-}
+    // Create booked rooms select dropdown
+    var roomsBooked = "<form> <select id='mySelect'>";
+    for (var i = 0; i < this.roomNumbersBooked.length; i++) {
 
+        roomsBooked += '<option value="' + this.roomNumbersBooked[i] + '">' + this.roomNumbersBooked[i] + "</option>";
+    }
+    roomsBooked += "</select> </form>";
 
+    document.getElementById("roomsBooked").innerHTML = roomsBooked;
+    
+    
+//      // Remove item from dropdown
+//     var roomPickedRemove = document.getElementById("mySelect");
+//     roomPickedRemove.remove(roomPickedRemove.selectedIndex);
+// }
+
+// Create the dropdown
 var rmList = "<form> <select id='mySelect'>";
 for (var i=0; i < hotel.roomNumbersAvailable.length; i++) {
     
